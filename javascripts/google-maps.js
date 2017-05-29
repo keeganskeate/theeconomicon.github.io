b352c3ba7a07efebf439087245e1d19e91b8d013
Keegan Skeate <keeganskeate@gmail.com>
2017-05-28 20:08:40 -0400

    Update google-maps.js

--- javascripts/google-maps.js
+++ javascripts/google-maps.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($){
 
 	//google map custom marker icon - .png fallback for IE11
 	var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-	var marker_url = ( is_internetExplorer11 ) ? 'img/cd-icon-location.png' : 'img/cd-icon-location.svg';
+	var marker_url = ( is_internetExplorer11 ) ? '../images/cd-icon-location.png' : '../images/cd-icon-location.svg';
 		
 	//define the basic color of your map, plus a value for saturation and brightness
 	var	main_color = '#2d313f',
