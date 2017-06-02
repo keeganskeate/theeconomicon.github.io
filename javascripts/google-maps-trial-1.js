962a52407b017f4fe58f7b5048f2497e594e7a5a
Keegan Skeate <keeganskeate@gmail.com>
2017-06-01 20:43:54 -0400

    Update google-maps-trial-1.js

--- javascripts/google-maps-trial-1.js
+++ javascripts/google-maps-trial-1.js
@@ -12,12 +12,30 @@ var customIcons = {
     }
 };
 var map;
+var	main_color = '#2d313f',
+		saturation_value= -20,
+		brightness_value= 5;
+var style= [{
+			featureType: "water",
+			elementType: "geometry",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		}
+	];
 function load() {
-    map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(41.613889, -72.7725),
         zoom: 9,
-        mapTypeId: 'terrain'
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: false,
+        mapTypeControl: false,
+        styles: style
     });
+    //var mc = new MarkerClusterer(map);
     var infoWindow = new google.maps.InfoWindow();
     // downloadUrl("phpsqlajax_genxml.php", function(data) {
     var xml = parseXml(xmlStr); // data.responseXML;
@@ -49,19 +67,21 @@ function bindInfoWindow(marker, map, infoWindow, html) {
     google.maps.event.addListener(marker, 'click', function () {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
+        google.maps.event.addListener(map, 'click', function() {
+    			infoWindow.close();
+    		});
     });
+    
 }
 
 function downloadUrl(url, callback) {
     var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest;
-
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
             request.onreadystatechange = doNothing;
             callback(request, request.status);
         }
     };
-
     request.open('GET', url, true);
     request.send(null);
 }
