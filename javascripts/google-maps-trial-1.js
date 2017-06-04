cf9efb73639d1281a1c36e1ad56d73813c273e5c
Keegan Skeate <keeganskeate@gmail.com>
2017-06-03 21:25:42 -0400

    current working map

--- javascripts/google-maps-trial-1.js
+++ javascripts/google-maps-trial-1.js
@@ -1,128 +1,127 @@
+var points = [
+{"name":"Business1",
+"lat":"39.718525","lng":"-105.017281",
+"centerType":"medical_center",
+"typeId":"1",
+},
+{"name":"Business2","lat":"39.679337","lng":"-104.987891","typeId":"1", "centerType":"medical_center"},{"name":"Business3","lat":"38.330489","lng":"-104.716092","typeId":"2","centerType":"retail_store"},{"name":"Business4","lat":"39.750626","lng":"-105.002313","typeId":"2","centerType":"retail_store"},{"name":"Business5","lat":"39.752584","lng":"-104.991667","typeId":"2","centerType":"retail_store"},{"name":"Business6","lat":"39.7114780","lng":"-105.000288","typeId":"1","centerType":"medical_center"}];
 var map;
-var	main_color = '#2d313f',
-		saturation_value= -20,
-		brightness_value= 5;
-var style= [{
-			featureType: "water",
-			elementType: "geometry",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		}
-	];
-var markerGroups = {
-    "medical_center": [],
-    "retail_store": [],
-};
-var customIcons = {
-    medical_center: {
-        icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
-    },
-    retail_store: {
-        icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
-    }
-};
-function load() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(39.00, -105.547222),
+var infowindow;
+var image = [];
+var gmarkers = [];
+var clusterMarkers = [];
+// Google Map
+function mapInit(){
+		// Map Options
+    var mapOptions = {
         zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        streetViewControl: false,
-        mapTypeControl: false,
-        styles: style
-    });
-    var infoWindow = new google.maps.InfoWindow();
-    var xml = parseXml(xmlStr);
-    var markers = xml.documentElement.getElementsByTagName("marker");
-    var bounds = new google.maps.LatLngBounds();
-    var trialmarkers = [];
-    for (var i = 0; i < markers.length; i++) {
-        var DBA = markers[i].getAttribute("DBA");
-        var licensee = markers[i].getAttribute("licensee");
-        var center_type = markers[i].getAttribute("center_type");
-        var license_number = markers[i].getAttribute("license_number");
-        var street = markers[i].getAttribute("street");
-        var city = markers[i].getAttribute("city");
-        var point = new google.maps.LatLng(
-        	parseFloat(markers[i].getAttribute("lat")),
-        	parseFloat(markers[i].getAttribute("lng")));
-        	bounds.extend(point);
-        var type = markers[i].getAttribute("type");
-        var html = "<b>" + DBA + "<br/>" + licensee + "</b> <br/>" + center_type + "<br/>" + license_number + "<br/>" + street + "<br/>" + city;
-        var icon = customIcons[type] || {};
-        var marker = new google.maps.Marker({
-            map: map,
-            position: point,
-            icon: icon.icon
-        });
-        markerGroups[type].push(marker);
-        trialmarkers.push(marker);
-        bindInfoWindow(marker, map, infoWindow, html);  
-    }
-    // Cluster Markers
-    var mcOptions = {
-      styles:[{url: "https://googlemaps.github.io/js-marker-clusterer/images/m1.png", width: 53, height:53, textColor:"#fff"}],
-      imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'};
-      //var markerCluster = new MarkerClusterer(map, trialmarkers, mcOptions);
-    var medical_center_markerCluster = new MarkerClusterer(map, markerGroups["medical_center"], mcOptions);
-    var retail_store_markerCluster = new MarkerClusterer(map, markerGroups["retail_store"], mcOptions);
-    // Set Bounds
-    map.fitBounds(bounds);
-}
-function bindInfoWindow(marker, map, infoWindow, html) {
-		// Attach info
-    google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.setContent(html);
-        infoWindow.open(map, marker);
-        // Close info on map click
-        google.maps.event.addListener(map, 'click', function() {
-    			infoWindow.close();
-    		});
-    });   
-}
-function toggleGroup(type) {
-    for (var i = 0; i < markerGroups[type].length; i++) {
-        var marker = markerGroups[type][i];
-        if (!marker.getVisible()) {
-            marker.setVisible(true);
-        } else {
-            marker.setVisible(false);
-        }
-    }
-}
-google.maps.event.addDomListener(window, 'load', load);
-function doNothing() {}
-function downloadUrl(url, callback) {
-    var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest;
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-        }
+				center: new google.maps.LatLng(39.00, -105.547222),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    request.open('GET', url, true);
-    request.send(null);
-}
-function parseXml(str) {
-    if (window.ActiveXObject) {
-        var doc = new ActiveXObject('MicrosoftXMLDOM');
-        doc.loadXML(str);
-        return doc;
-    } else if (window.DOMParser) {
-        return (new DOMParser).parseFromString(str, 'text/xml');
+    // Cluster Options
+    var clusterOptions = {
+    	gridSize: 50,
+      maxZoom: 10,
+      minimumClusterSize: 5,
+      styles:[{
+      	url: "https://googlemaps.github.io/js-marker-clusterer/images/m1.png",
+        width: 53,
+        height:53,
+        textColor:"#fff",
+        textSize: 11,
+        }]
+    };
+    // Import marker images
+		var customIcons = {
+    	medical_center: {
+        icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
+    	},
+    	retail_store: {
+        icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
+    	}
+		};
+    // Create Map
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		// Close info on map click
+    google.maps.event.addListener(map, 'click', function() {
+      infowindow.close();
+    });
+    // Create markers and clusters
+    addLocation();
+    var markerCluster = new MarkerClusterer(map, clusterMarkers, clusterOptions);
+    function addLocation(place,category) {
+      for (var x in points){
+          var development = points[x];
+          var location = new google.maps.LatLng(development.lat, development.lng);
+          storeMarker(location, development);
+      }   
     }
-}
+    function storeMarker(location, development){
+      var latLng = location;
+      var icon = customIcons[development.centerType] || {};
+      // Marker Options
+      var storedmarker = new google.maps.Marker({
+            position: latLng,
+            icon: icon.icon,
+            title: development.name
+      });
+   		// Categories
+      storedmarker.typeCategory = development.typeId;
+			// Info Windows
+      google.maps.event.addListener(storedmarker, 'click', function() {
+        if(typeof infowindow != 'undefined') infowindow.close();
+        // Info Window Content
+        infowindow = new google.maps.InfoWindow({
+          content: "<b>"+ development.name +  "</b><br/>"
+        })
+        
+        // Optional change to infobubble
+        //var  infowindow = new InfoBubble({maxWidth:300});
+      //infowindow.addTab('Tab 1',"<B> Trial </B>");
+  		//infowindow.addTab('Tab 2',"<B> Trial </B>");
+      
+      // Close Infowindow on second click
+      if (!storedmarker.open) {
+      	infowindow.open(map, storedmarker);
+      	storedmarker.open = true;
+      }
+      else{
+      	infowindow.close();
+      	storedmarker.open = false;
+      }
+        //infowindow.open(map, storedmarker);
+      });    
+			// Add marker to cluster
+      clusterMarkers.push(storedmarker);    
+    }
 
-var xmlStr = '<markers>' +
-'<marker type="medical_center" lat="39.718525" lng="-105.017281" DBA="Business1" licensee="licensee" center_type="Medical Center" license_number="" street = "Street" city="City, CO ZIP" />'+
-'<marker type="medical_center" lat="39.679337" lng="-104.987891" DBA="Business2" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'<marker type="medical_center" lat="38.330489" lng="-104.716092" DBA="Business3" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'<marker type="retail_store" lat="39.711478" lng="-105.002313" DBA="Business4" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'<marker type="retail_store" lat="39.752584" lng="-104.991667" DBA="Business5" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'<marker type="retail_store" lat="39.750626" lng="-105.000288" DBA="Business6" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'<marker type="retail_store" lat="39.5" lng="-104.5" DBA="Business7" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'<marker type="retail_store" lat="38.5" lng="-104.25" DBA="Business8" licensee="licensee" center_type="Medical Center" license_number="#" street = "Street #" city="City, CO ZIP" />'+
-'</markers>';
+		// Toggle for Categories
+    function toggle(category,show) { 
+      var markers=[];
+      for (var i=0; i<clusterMarkers.length; i++) {
+        if (clusterMarkers[i].typeCategory == category) {
+          markers.push(clusterMarkers[i]);
+          clusterMarkers[i].setVisible(show);
+        }
+      }
+      if(markers.length){
+        markerCluster[(show)?'addMarkers':'removeMarkers'](markers);
+      }
+      if(!show && infowindow)infowindow.close();
+    }
+    function select(box,category) {
+        toggle(category,box.checked);
+    }
+// Attach value of type
+jQuery(document).ready(function($) {
+  $('.map-filter').click(function () {
+    select(this, this.value);
+  });
+});
+
+}
+// Execute
+jQuery(document).ready(function(){
+    mapInit();
+});
+  
