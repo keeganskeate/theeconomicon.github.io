342e79c0e6fe3aff04e137c09b7e4f662b08b9d3
Keegan Skeate <keeganskeate@gmail.com>
2017-09-06 19:31:35 -0400

    Update stickyHeader.js

--- javascripts/stickyHeader.js
+++ javascripts/stickyHeader.js
@@ -3,12 +3,11 @@ jQuery(document).ready(function($) {
 $(function(){
 	$("header").before($(".StickyHeader").clone().addClass("fixed"));
 	$(window).scroll(function(){
-	if($(window).scrollTop() >= 150){
-	    $('.StickyHeader.fixed').addClass('slideDown');
-	}
+		if($(window).scrollTop() >= 150){
+			$('.StickyHeader.fixed').addClass('slideDown');
+		}
 	else{
 		$('.StickyHeader.fixed').removeClass('slideDown');
 	}
-});
 
 });
