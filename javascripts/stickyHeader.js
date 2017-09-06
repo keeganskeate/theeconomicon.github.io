50683b519158ec67d41f014d78e41c3bc4a7240b
Keegan Skeate <keeganskeate@gmail.com>
2017-09-06 14:41:26 -0400

    Update stickyHeader.js

--- javascripts/stickyHeader.js
+++ javascripts/stickyHeader.js
@@ -1,6 +1,6 @@
 /* Fixed header slide down on scroll */
-jQuery(document).ready(function($) {
-    $("header").before($("header").clone().addClass("animateIt"));
+$(document).ready(function () {
+    $("sticky").before($("sticky").clone().addClass("animateIt"));
     $(window).on("scroll", function () {
         $("body").toggleClass("down", ($(window).scrollTop() > 100));
     });
