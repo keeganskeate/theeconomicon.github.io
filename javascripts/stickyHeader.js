368472ba3a0f70b5045c3dbf0c7d80e17d39dfde
Keegan Skeate <keeganskeate@gmail.com>
2017-09-06 19:48:18 -0400

    Update stickyHeader.js

--- javascripts/stickyHeader.js
+++ javascripts/stickyHeader.js
@@ -1,13 +1,14 @@
 /* Fixed header slide down on scroll */
 jQuery(document).ready(function($) {
-$(function(){
-	$("header").before($(".StickyHeader").clone().addClass("fixed"));
-	$(window).scroll(function(){
-		if($(window).scrollTop() >= 150){
-			$('.StickyHeader.fixed').addClass('slideDown');
-		}
-	else{
-		$('.StickyHeader.fixed').removeClass('slideDown');
-	}
-
+	$(function(){
+		$("header").before($(".StickyHeader").clone().addClass("fixed"));
+		$(window).scroll(function(){
+			if($(window).scrollTop() >= 150){
+				$('.StickyHeader.fixed').addClass('slideDown');
+			}
+			else{
+				$('.StickyHeader.fixed').removeClass('slideDown');
+			}
+		});
+	});
 });
