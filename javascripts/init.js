7414311ba453ef4fb95f3977354b48cab98e620f
Keegan Skeate <keeganskeate@gmail.com>
2017-09-08 09:00:59 -0400

    Update init.js

--- javascripts/init.js
+++ javascripts/init.js
@@ -74,8 +74,8 @@ Javascript Core Functions
 	
 	// Back to top Button
 	jQuery(document).ready(function() {
-		var offset = 250;
-		var duration = 300;
+		var offset = 250; // The offset is when the button will appears.
+		var duration = 300; // The duration is the time until the button will appear in milliseconds.
 		jQuery(window).scroll(function() {
 			if (jQuery(this).scrollTop() > offset) {
 				jQuery(‘.back-to-top’).fadeIn(duration);
