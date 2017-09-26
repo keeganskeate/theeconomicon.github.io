f190ab875a490078b9857d54c564139d0a39a5a4
Keegan Skeate <keeganskeate@gmail.com>
2017-09-25 21:45:08 -0400

    deleted unused narrow class change at 840

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,7 +12,6 @@
 			wide:		{ range: '-1680', href: 'stylesheets/style-wide.css', containers: 1200, grid: { gutters: 40 } },
 			normal:		{ range: '-1280', href: 'stylesheets/style-normal.css', containers: 960, lockViewport: true },
 			narrow:		{ range: '-1028', href: 'stylesheets/style-narrow.css', containers: '95%', grid: { gutters: 25 } },
-			//narrower:	{ range: '-840', href: 'stylesheets/style-narrower.css', grid: { collapse: true } },
 			narrower:	{ range: '-980', href: 'stylesheets/style-narrower.css', grid: { collapse: true } },
 			mobile:		{ range: '-640', href: 'stylesheets/style-mobile.css', containers: '90%', grid: { gutters: 15 } }
 		}
