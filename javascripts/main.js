df13a4c1f61498a50d903dcca5f6235a804896d9
Keegan Skeate <keeganskeate@gmail.com>
2017-11-01 21:14:21 -0400

    added jquery lazy class

--- javascripts/main.js
+++ javascripts/main.js
@@ -53,6 +53,11 @@
 			}
 		});
 	});
+	
+	// Lazy loading for images
+	$(function() {
+		$('.lazy').lazy();
+	});
 
 	// Disable animations/transitions until page has loaded.
 	$(function() {
@@ -76,18 +81,18 @@
 
 // Back-to-top Button
 jQuery(document).ready(function() {
-		var offset = 250; // The offset is when the button will appears.
-		var duration = 300; // The duration is the time until the button will appear in milliseconds.
-		jQuery(window).scroll(function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.back-to-top').fadeIn(duration);
-			} else {
-				jQuery('.back-to-top').fadeOut(duration);
-			}
-		});
-		jQuery('.back-to-top').click(function(event) {
-			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		});
+	var offset = 250; // The offset is when the button will appears.
+	var duration = 300; // The duration is the time until the button will appear in milliseconds.
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > offset) {
+			jQuery('.back-to-top').fadeIn(duration);
+		} else {
+			jQuery('.back-to-top').fadeOut(duration);
+		}
+	});
+	jQuery('.back-to-top').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	});
 });
