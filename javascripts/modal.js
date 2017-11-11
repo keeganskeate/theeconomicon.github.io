850547e0af65062d3609c96675859e8c48a5ad68
Keegan Skeate <keeganskeate@gmail.com>
2017-11-11 11:48:41 -0500

    Update modal.js

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -1,9 +1,9 @@
 // Back-to-top Button
 jQuery(document).ready(function() {
     jQuery('.single-popup').magnificPopup({ 
-				type: 'image',
-				removalDelay: 300,
-				mainClass: 'mfp-with-fade',
-				closeOnContentClick: true
-			});
+	type: 'image',
+	removalDelay: 300,
+	mainClass: 'mfp-with-fade',
+	closeOnContentClick: true
+	});
 });
